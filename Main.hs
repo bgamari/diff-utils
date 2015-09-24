12ca351057f1c5ad6ea50028ea34c2ae75fdab22
Ben Gamari <ben@smart-cactus.org>
2015-09-24 16:51:16 +0200

    Add option parsing

--- Main.hs
+++ Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Attoparsec.Text
+import Data.Attoparsec.Text (parseOnly)
+import Data.Maybe
 import qualified Data.Text.IO as TIO
 import Control.Applicative
+import Options.Applicative
 import Lucid
 
 import Diff2Html
@@ -22,4 +24,17 @@ chunksToHtml chunks =
         head_ $ style_ [] styleSheet
         body_ $ chunksToTable chunks
 
-main = return ()
+data Options = Options { patchFile :: FilePath
+                       , outputFile :: Maybe FilePath
+                       }
+
+options :: Parser Options
+options = Options <$> strArgument (help "Patch file")
+                  <*> optional (option auto (help "HTML output"))
+
+main :: IO ()
+main = do
+    opts <- execParser $ info (helper <*> options) mempty
+    chunks <- parseChunks (patchFile opts)
+    let out = fromMaybe (patchFile opts++".html") (outputFile opts)
+    renderToFile out $ chunksToHtml chunks
